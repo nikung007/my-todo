6b0ff7529f3ac82bca449a4c747b6af568f3fb5e
nikung007 <105716681+nikung007@users.noreply.github.com>
2022-06-17 10:56:48 +0530

    Start

---
+++ src/componets/topbar/Topbar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Topbar.css'
+class Topbar extends Component {
+    render() {
+        return (
+            <div>
+                <div className="todoTopbar">
+                    <h1>Todo List</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Topbar;
